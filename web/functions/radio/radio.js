4113212964635757ace2182995785210bc7f5030
Thai on Cloud9 <dtinth@spacet.me>
2019-09-05 13:44:28 +0000

    Add Netlify

---
+++ web/functions/radio/radio.js
@@ -0,0 +1,12 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+exports.handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` })
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}

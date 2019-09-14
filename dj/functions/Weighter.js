75558a9ce564544bfc92e081151fb5a4748af9b7
Thai on Cloud9 <dtinth@spacet.me>
2019-09-14 06:23:24 +0000

    Create weighter

---
+++ dj/functions/Weighter.js
@@ -0,0 +1,15 @@
+const Bsearch = require('bsearch')
+/**
+ * @param {number[]} weights
+ */
+exports.createWeightedIndexer = function createWeightedIndexer(weights) {
+  /** @type {number[]} */
+  const data = []
+  let cumulative = 0
+  let total = weights.reduce((a, w) => a + w, 0)
+  weights.forEach(w => {
+    data.push(cumulative / total)
+    cumulative += w
+  })
+  return Bsearch(data)
+}

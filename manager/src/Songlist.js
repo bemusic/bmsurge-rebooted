243be6c15916191cf66ac05596f39f8abd31521a
Thai on Cloud9 <dtinth@spacet.me>
2019-09-14 05:49:50 +0000

    Include event and entry information in the songlist

--- manager/src/Songlist.js
+++ manager/src/Songlist.js
@@ -10,9 +10,33 @@ exports.generateSonglist = async function generateSonglist(client) {
       disabled: { $ne: true }
     })
     .toArray()
+  const eventMap = new Map(
+    (await client
+      .db()
+      .collection('events')
+      .find({})
+      .toArray()).map(e => [
+      e._id,
+      {
+        ...e,
+        entryMap: new Map((e.entries || []).map(n => [n.entryId, n]))
+      }
+    ])
+  )
   const updatedTimeMap = new Map(songs.map(s => [String(s._id), s.renderedAt]))
   const songlist = songs.map(s => {
     const chart = s.renderResult.selectedChart
+    const entryInfo = {}
+    const event = eventMap.get(s.eventId)
+    if (event) {
+      entryInfo.eventTitle = event.title
+      entryInfo.eventUrl = event.url
+      const entry = s.entryId && event.entryMap.get(s.entryId)
+      if (entry) {
+        entryInfo.entryId = s.entryId
+        entryInfo.entryUrl = entry.url
+      }
+    }
     return {
       songId: String(s._id),
       fileId: s.renderResult.operationId,
@@ -22,7 +46,8 @@ exports.generateSonglist = async function generateSonglist(client) {
       md5: chart.md5,
       duration: s.renderResult.wavSizeAfterTrimEnd / (44100 * 2 * 2),
       event: s.eventId,
-      updatedAt: s.renderedAt
+      updatedAt: s.renderedAt,
+      ...entryInfo
     }
   })
   return { updatedTimeMap, songlist }

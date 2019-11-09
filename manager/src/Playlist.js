d7f28c1c522c4b8d7f7bdfa1bc82e38e488b69f3
Thai on Cloud9 <dtinth@spacet.me>
2019-11-09 13:56:19 +0000

    Add entryId to m3u

--- manager/src/Playlist.js
+++ manager/src/Playlist.js
@@ -7,14 +7,20 @@ exports.generatePlaylist = async function generatePlaylist(client, args = {}) {
   const filters = {}
   if (args.eventId) filters.eventId = String(args.eventId)
   const found = await songsCollection
-    .find({ 'renderResult.uploadedAt': { $exists: true }, ...filters })
+    .find({
+      'renderResult.uploadedAt': { $exists: true },
+      disabled: { $ne: true },
+      ...filters
+    })
     .toArray()
   const out = []
   out.push('#EXTM3U')
+  found.sort((a, b) => a.entryId - b.entryId)
   for (const song of found) {
     const chart = song.renderResult.selectedChart
+    const entryPrefix = song.entryId ? `${song.entryId}. ` : ''
     out.push(
-      `#EXTINF:${Math.floor(chart.duration)},[${chart.info.genre}] ${
+      `#EXTINF:${Math.floor(chart.duration)},${entryPrefix}[${chart.info.genre}] ${
         chart.info.artist
       } - ${chart.info.title} #${song.eventId}`
     )
